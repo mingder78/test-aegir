f4e0878b97545d5fadead8050ab1b1ce8b2db212
mingder wang <mingderwang@me.com>
2026-03-09 06:24:51 +0800

    fix:ok2

--- src/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export interface HelloProps {
-  name: string;
-}
-
-export function Hello({ name }: HelloProps) {
-  return <div>Hello {name}</div>;
-}
--- test/hello.spec.tsx
+++
@@ -1,7 +0,0 @@
-import { expect } from 'aegir/chai'
-
-describe('browser test', () => {
-  it('works in browser', () => {
-    expect(1 + 1).to.equal(2)
-  })
-})
