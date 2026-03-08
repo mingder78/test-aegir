7c46d0c40dbce0a13f61b42d29f939ec5dab182a
mingder wang <mingderwang@me.com>
2026-03-09 04:44:29 +0800

    feat: release test

--- .aegir.cjs
+++ .aegir.cjs
@@ -2,5 +2,11 @@ module.exports = {
   tsRepo: true,
   test: {
     build: false  // don't build test files
+  },
+  release: {
+    npmPublish: true,
+    npmPublishOptions: {
+      provenance: false
+    }
   }
 }
