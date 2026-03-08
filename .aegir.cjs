b1d3b606a667362a1262faa521c949ec8838fb70
mingder wang <mingderwang@me.com>
2026-03-09 04:25:15 +0800

    fix: remove test-node

--- .aegir.cjs
+++ .aegir.cjs
@@ -1,10 +1,6 @@
 module.exports = {
   tsRepo: true,
-  release: {
-    build: false
-  },
   test: {
-    runner: 'bun', // change this to use bun
     build: false  // don't build test files
   }
 }
